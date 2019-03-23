b09643a291eef05beee120e8d7d70c86d2f72fc8
Angeles_Octaviano <37378856+angelesOct@users.noreply.github.com>
2019-03-23 00:04:56 -0600

    se añade la pagina a heroku

--- application/models/config.py
+++ application/models/config.py
@@ -1,9 +1,9 @@
 import web
 
-db_host = 'localhost'
-db_name = 'ferreteriamago'
-db_user = 'ferreteria'
-db_pw = 'ferreteria.2019'
+db_host = 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'myikjhhyc5i3f2rx'
+db_user = 'm6fcoejy25ptsim8'
+db_pw = 'y8bm4dw6i4knp9lq'
 
 db = web.database(
     dbn='mysql',
@@ -11,4 +11,4 @@ db = web.database(
     db=db_name,
     user=db_user,
     pw=db_pw
-    )
+    ) 
